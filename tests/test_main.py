cbe363636def6a5dfe5dc884d836f520e19b1689
Victor Santoscoy <victor.santoscoy1731@gmail.com>
2023-04-22 15:00:12 -0600

    Adding integration test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
-import utils
 from main import app
 from unittest.mock import patch
 from fastapi.testclient import TestClient
+
 
 client = TestClient(app)
 
@@ -13,14 +13,22 @@ def test_read_main():
 
 @patch("utils.get_berries_names")
 @patch("utils.get_growth_times")
-def test_get_berries_stats():
+def test_get_berries_stats(mock_get_growth_times, mock_get_berries_names):
     mock_get_berries_names.return_value = ["Berry A", "Berry B", "Berry C"]
-    mock_get_growth_times.return_value = [1, 3, 5, 7]
+    mock_get_growth_times.return_value = [1, 2, 3, 4, 5]
 
     response = client.get("/allBerryStats")
     assert response.status_code == 200
 
-    key_list = ["berries_names", "min_growth_time", "median_growth_time", "max_growth_time", "variance_growth_time",
-                "mean_growth_time", "frequency_growth_time"]
-    for key in response.json().keys():
-        assert key in key_list
+    expected_response = {
+        "berries_names": ["Berry A", "Berry B", "Berry C"],
+        "min_growth_time": 1,
+        "median_growth_time": 3,
+        "max_growth_time": 5,
+        "variance_growth_time": 2.5,
+        "mean_growth_time": 3,
+        "frequency_growth_time": {"1": 1, "2": 1, "3": 1, "4": 1, "5": 1}
+    }
+    for key, val in response.json().items():
+        assert key in expected_response.keys()
+        assert val == expected_response[key]
